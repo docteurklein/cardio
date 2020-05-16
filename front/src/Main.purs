53e7619d9915025b7ea81dbb755371f7f7eb965e
Florian Klein <florian.klein@free.fr>
2020-05-16 16:16:01 +0200

    front

---
+++ front/src/Main.purs
@@ -0,0 +1,13 @@
+module Main where
+
+import Prelude
+
+import Cardio.Card as Card
+import Effect (Effect)
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI Card.component unit body
