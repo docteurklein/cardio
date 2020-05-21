5d7e1d092cb7e869627d8cdd387ce5976265275e
Florian Klein <florian.klein@free.fr>
2020-05-21 22:46:04 +0200

    projection

--- sql/example.sql
+++ sql/example.sql
@@ -1,22 +1,22 @@
 set search_path = cardio;
 
 -- which card
-select card_id, name from card where card_id = :'card_id';
+select card_id, title from card where card_id = :'card_id';
 
 -- find all descendants of :card_id:
-select card_id, name descendant from card_with_ancestors where ancestors @> array[:'card_id']::uuid[];
+select card_id, title descendant from card_with_ancestors where ancestors @> array[:'card_id']::uuid[];
 
 -- find all ancestors of :card_id:
-select card_id, name ancestor from card where card_id in (select unnest(ancestors) from card_with_ancestors where card_id = :'card_id');
+select card_id, title ancestor from card where card_id in (select unnest(ancestors) from card_with_ancestors where card_id = :'card_id');
 
 -- which layer
-select layer_id, name from layer where layer_id = :'layer_id';
+select layer_id, title from layer where layer_id = :'layer_id';
 
 -- find all descendants of :layer_id:
-select layer_id, name descendant from layer_with_ancestors where ancestors @> array[:'layer_id']::uuid[];
+select layer_id, title descendant from layer_with_ancestors where ancestors @> array[:'layer_id']::uuid[];
 
 -- find all ancestors of :layer_id:
-select layer_id, name ancestor from layer where layer_id in (select unnest(ancestors) from layer_with_ancestors where layer_id = :'layer_id');
+select layer_id, title ancestor from layer where layer_id in (select unnest(ancestors) from layer_with_ancestors where layer_id = :'layer_id');
 
 -- find all cards of a layer
-select card_id, card.name, layer.name from card join card_in_layer using (card_id) join layer using (layer_id) where layer_id = :'layer_id';
+select card_id, card.title, layer.title from card join card_in_layer using (card_id) join layer using (layer_id) where layer_id = :'layer_id';
--- sql/schema.sql
+++ sql/schema.sql
@@ -9,7 +9,7 @@ language sql parallel safe strict as $$
 select extensions.uuid_generate_v4();
 $$;
 
-create table if not exists migration (id bigint primary key, at timestamptz not null default clock_timestamp());
+create table if not exists migration (id bigint primary key, reason text not null, at timestamptz not null default clock_timestamp());
 
 create or replace function migrate() returns bigint
 language plpgsql as $_$
@@ -19,7 +19,6 @@ begin
     raise notice 'current "%"', current;
 
     case when current < 1 then
-
         create table card (
             card_id uuid primary key default uuid4(),
             parent_id uuid references card (card_id),
@@ -39,12 +38,11 @@ begin
             layer_id uuid references layer (layer_id)
         );
 
-        insert into migration (id) values (1);
+        insert into migration (id, reason) values (1, 'initial schema for card and layer');
     else null;
     end case;
 
     case when current < 2 then
-
         create recursive view card_with_ancestors (card_id, parent_id, name, description, ancestors, level) as
             select card_id, parent_id, name, description, '{}'::uuid[], 1
             from card
@@ -65,12 +63,11 @@ begin
             join layer_with_ancestors p on (p.layer_id = sub.parent_id)
         ;
 
-        insert into migration (id) values (2);
+        insert into migration (id, reason) values (2, 'recursive views to query graph');
     else null;
     end case;
 
     case when current < 3 then
-
         create role web_anon nologin;
 
         grant usage on schema cardio to web_anon;
@@ -80,33 +77,52 @@ begin
         grant select on cardio.card_with_ancestors to web_anon;
         grant select on cardio.layer_with_ancestors to web_anon;
 
-        insert into migration (id) values (3);
+        insert into migration (id, reason) values (3, 'add roles for postgrest');
     else null;
     end case;
 
     case when current < 4 then
-
         alter table card rename column name to title;
         alter table layer rename column name to title;
 
-        insert into migration (id) values (4);
+        drop view card_with_ancestors;
+        create recursive view card_with_ancestors (card_id, parent_id, title, description, ancestors, level) as
+            select card_id, parent_id, title, description, '{}'::uuid[], 1
+            from card
+            where parent_id is null
+            union all
+            select sub.card_id, sub.parent_id, sub.title, sub.description, ancestors || sub.parent_id, level + 1
+            from card sub
+            join card_with_ancestors p on (p.card_id = sub.parent_id)
+        ;
+
+        drop view layer_with_ancestors;
+        create or replace recursive view layer_with_ancestors (layer_id, parent_id, title, description, ancestors, level) as
+            select layer_id, parent_id, title, description, '{}'::uuid[], 1
+            from layer
+            where parent_id is null
+            union all
+            select sub.layer_id, sub.parent_id, sub.title, sub.description, ancestors || sub.parent_id, level + 1
+            from layer sub
+            join layer_with_ancestors p on (p.layer_id = sub.parent_id)
+        ;
+
+        insert into migration (id, reason) values (4, 'rename title column');
     else null;
     end case;
 
     case when current < 5 then
-
         alter table card add column created_at timestamptz not null default clock_timestamp();
         alter table card add column updated_at timestamptz not null default clock_timestamp();
 
         alter table layer add column created_at timestamptz not null default clock_timestamp();
         alter table layer add column updated_at timestamptz not null default clock_timestamp();
 
-        insert into migration (id) values (5);
+        insert into migration (id, reason) values (5, 'add edit timestamps');
     else null;
     end case;
 
     case when current < 6 then
-
         create table message (
             message_id uuid primary key default uuid4(),
             type text not null,
@@ -120,26 +136,8 @@ begin
         create rule immutable_message as on update to message do instead nothing;
         create rule immortal_message as on delete to message do instead nothing;
 
-        create or replace function project(message message) returns void
-        language plpgsql as $$
-        begin
-
-            perform pg_notify(topic::text, json_build_object(
-                'statement', 'select * from cardio.message where message_id = $1',
-                'params', array[message.message_id]
-            )::text) from unnest(message.topics || array['message_added', message.type]) as topic;
-
-            case message.type
-
-                when 'card_created' then
-                    insert into card
-                    (card_id              ,  title                     ,  description                     ,  created_at,  updated_at) values
-                    (message.aggregate_id ,  message.payload->>'title' ,  message.payload->>'description' ,  message.at,  message.at);
-                else
-                    raise notice 'no projection for message "%"', message.type;
-            end case;
-        end;
-        $$;
+        grant insert on cardio.message to web_anon;
+        grant select on cardio.message to web_anon;
 
         create or replace function trigger_projection() returns trigger
         language plpgsql as $$
@@ -152,7 +150,24 @@ begin
         create trigger on_message_insert after insert on message
         for each row execute function trigger_projection();
 
-        insert into migration (id) values (6);
+        insert into migration (id, reason) values (6, 'event sourcing');
+    else null;
+    end case;
+
+    case when current < 7 then
+        alter table card_in_layer add column added_at timestamptz not null default clock_timestamp();
+
+        create or replace function trigger_projection() returns trigger
+        language plpgsql as $$
+        begin
+            perform project_v2(new);
+            return null;
+        end;
+        $$;
+
+        drop function if exists project(message);
+
+        insert into migration (id, reason) values (7, 'adapt projection to add card_in_layer timestamp');
     else null;
     end case;
 
@@ -162,5 +177,32 @@ end;
 $_$;
 
 select migrate() as current;
+
+create or replace function project_v2(message message) returns void
+language plpgsql as $$
+begin
+    perform pg_notify(topic, json_build_object(
+        'statement', 'select * from cardio.message where message_id = $1',
+        'params', array[message.message_id]
+    )::text) from unnest(message.topics || array['message_added', message.type, '*']) as topic;
+
+    case message.type
+        when 'card_created' then
+            insert into card
+            (card_id              ,  title                     ,  description                     ,  created_at,  updated_at) values
+            (message.aggregate_id ,  message.payload->>'title' ,  message.payload->>'description' ,  message.at,  message.at);
+        when 'layer_created' then
+            insert into layer
+            (layer_id             ,  title                     ,  description                     ,  created_at,  updated_at) values
+            (message.aggregate_id ,  message.payload->>'title' ,  message.payload->>'description' ,  message.at,  message.at);
+        when 'card_added_to_layer' then
+            insert into card_in_layer
+            (card_id              ,  layer_id                             , added_at) values
+            (message.aggregate_id ,  (message.payload->>'layer_id')::uuid , message.at);
+        else
+            raise notice 'no projection for message "%"', message.type;
+    end case;
+end;
+$$;
 
 commit;
